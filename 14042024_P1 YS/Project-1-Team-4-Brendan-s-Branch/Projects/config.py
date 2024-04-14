26972b7902553788a2472c0b5a4349b7b2199979
Y1U9M9A4I <situ.ym@gmail.com>
2024-04-14 17:04:53 -0400

    Backup Process

---
+++ 14042024_P1 YS/Project-1-Team-4-Brendan-s-Branch/Projects/config.py
@@ -0,0 +1,4 @@
+#  Add your API key
+api_key = "5dv4ix8pde8jz7xagucr3fa2b"
+# data.lacity ID = "5dv4ix8pde8jz7xagucr3fa2b"
+# data.lacity Key Secret = "f0btslkt53uvcx4fnq0h7f6b39qbicj69bim8hen8sl5m6p36"
